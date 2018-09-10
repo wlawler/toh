9ade8623c440fbfad3fb91bce2ea2802f717611e
wes lawler <wes.lawler2@gmail.com>
2018-09-10 16:25:34 -0400

    finished input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel
+
 
 @NgModule({
   declarations: [
