3687f13b3aaa7d1e543a3bb2e83cee981c73b220
wes lawler <wes.lawler2@gmail.com>
2018-09-11 17:36:05 -0400

    added list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name:'Dr. Radoslave'},
+    { id: 12, name:'The Bulgarian Bone Smasher'},
+    { id: 13, name:'Professor Giggles McBeard'},
+    { id: 14, name:'The Angular Avenger'},
+    { id: 15, name:'The Lone Coder'},
+    { id: 16, name:'Vim Victor'},
+    { id: 17, name:'The Grinding Grinner'},
+    { id: 18, name:'The Focused Phenom'},
+    { id: 19, name:'Captain Constant Coder'},
+
+];
