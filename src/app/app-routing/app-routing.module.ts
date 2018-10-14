53c8e7dc06a5bab22d661e9ac0e30c0de0ee2859
wes lawler <wes.lawler2@gmail.com>
2018-10-14 15:53:06 -0400

    preparing to add a gero detail route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule}  from '@angular/core'; 
 import { RouterModule, Routes, Router} from '@angular/router'; 
 import { HeroesComponent } from '../heroes/heroes.component';
+import { DashboardComponent } from '.dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent
-
-}
+  { path: 'dashboard', component:DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'heroes', component: HeroesComponent},
 ];
+
 @NgModule({
   imports:[ RouterModule.forRoot(routes)  ],
   exports:[ RouterModule]
