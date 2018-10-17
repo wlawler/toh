cda654e2746622904b42f05599f09f5a79a595de
wes lawler <wes.lawler2@gmail.com>
2018-10-17 17:09:13 -0400

    bug checked and finished the routing section of tour of heroes. one more to go

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    
+  ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,9 +12,14 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeroes(id: number): Observable<Hero> {
+  getHeroes(): Observable<Hero[]> {
     //TODO: send the message _after_ fetching heroes
-    this.messageService.add('HeroService: fetched heroes')
+    this.messageService.add('HeroService: fetched heroes');
+    return of (HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    //TODO: send the message _after_ fetching heroes
+    this.messageService.add('HeroService: fetched heroes id=${id}')
    return of (HEROES.find(hero => hero.id === id));
   }
 }
